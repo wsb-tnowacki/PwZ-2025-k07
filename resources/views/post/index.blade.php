b8ca8a620a70b7dd801fc327ee3e69e1b1825734
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2025-05-10 13:16:37 +0200

    dodanie przycisku usuwania postu na liście

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -14,9 +14,10 @@
                 <th scope="col">Akcja</th>
             </thead>
             <tbody>
+                @php($lp=1)
                 @foreach ($posty as $post)
                     <tr>
-                        <td>{{$post->id}}</td>
+                        <td>{{$lp++}}</td>
                         <td><a href="{{route('post.show',$post->id)}}">{{$post->tytul}}</a></td>
                         <td>{{$post->autor}}</td>
                         <td>{{date('j F Y',strtotime($post->created_at))}}</td>
@@ -24,6 +25,11 @@
                             <a href="{{route('post.edit',$post->id)}}">
                                 <button class="btn btn-success m-1" type="submit">E</button>
                             </a>
+                            <form action="{{route('post.destroy', $post->id)}}" method="post" onsubmit="return confirm('Czy na pewno usunąć ten post?')">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger m-1" type="submit">X</button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
